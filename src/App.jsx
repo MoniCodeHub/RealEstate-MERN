c043fbfe266ea59dac12dbd42a47f7916d560629
moni <monishabaskararao@gmail.com>
2024-02-14 14:24:55 -0500

    created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import About from './pages/About';
 
 
 const App = () => {
   return (
-    <h1 className="text-red-600">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+
+    </BrowserRouter>
   )
-}
+};
 
 
 export default App
