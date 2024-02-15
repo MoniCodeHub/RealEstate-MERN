27fe63edfa11999812e3a948afa82a8d11aa5fbc
moni <monishabaskararao@gmail.com>
2024-02-15 10:25:09 -0500

    connected to mongoDB Atlas

--- server-api/index.js
+++ server-api/index.js
@@ -1,10 +1,24 @@
-import express from 'express';
+/** @format */
+
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 const PORT = 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT} successfully!!!!`);
+  console.log(`Server running on ${PORT} successfully!!!!`);
 });
-
