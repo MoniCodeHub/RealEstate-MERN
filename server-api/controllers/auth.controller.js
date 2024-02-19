d477ba90f6dac7392c0a5895771235e94daba9f1
moni <monishabaskararao@gmail.com>
2024-02-19 11:36:42 -0500

    Created Update User api route

--- server-api/controllers/auth.controller.js
+++ server-api/controllers/auth.controller.js
@@ -26,7 +26,7 @@ export const signin = async (req, res, next) => {
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Wrong Credentials!"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-    const { password: pass, ...rest } = validUser._doc; //we do this to hide the password being visible
+    const { password: pass, ...rest } = validUser._doc; //we do this to hide the password being visible in console
     res.cookie("access_token", token, { httpOnly: true }).status(200).json(rest);
   } catch (error) {
     next(error);
