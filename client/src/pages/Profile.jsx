3e182c9ede76f9b3b28ee47725d1512446ffbd83
moni <monishabaskararao@gmail.com>
2024-02-20 22:39:39 -0500

    Created Showlistings on user's profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -27,6 +27,8 @@ const Profile = () => {
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
   const [deleteSuccess, setDeleteSuccess] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
   const dispatch = useDispatch();
   // console.log(file);
   console.log(formData);
@@ -131,6 +133,22 @@ const handleSignOut = async () => {
     dispatch(signOutUserFailure(data.message));
   }
 }
+
+const handleShowListings = async () => {
+  try {
+    setShowListingsError(false);
+    const response = await fetch(`/api/user/listings/${currentUser._id}`);
+    const data = await response.json();
+    if(data.success === false){
+      setShowListingsError(true)
+      return;
+    }
+    setUserListings(data);
+    
+  } catch (error) {
+    setShowListingsError(true);
+  }
+}
   
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -188,7 +206,7 @@ const handleSignOut = async () => {
         className='bg-slate-700 text-white rounded-lg p-3 
         uppercase font-serif hover:opacity-95'
         >{loading ? 'Loading' : 'Update'}</button>
-        <Link to='/create-listing' className='bg-pink-300 font-serif font-semibold text-center 
+        <Link to='/create-listing' className='bg-pink-400 text-white font-serif font-semibold text-center 
         p-3 rounded-lg hover:opacity-95'>
             CREATE LISTING
         </Link>
@@ -199,8 +217,42 @@ const handleSignOut = async () => {
       </div><br/>
       <p className='text-red-600 font-semibold' >{error ? error: ''}</p>
       <p className='text-green-600 font-semibold' >{updateSuccess ? 'User Updated Sccessfully!' : ''}</p>
+
+      <button 
+          onClick={handleShowListings} 
+          className='text-green-700 font-serif font-bold w-full shadow-xl'
+          >Show Listings
+      </button>
+      <p className='text-red-700'>{showListingsError ? 'Error showing Listings' : '' }</p>
+
+      {userListings &&
+       userListings.length > 0 && 
+        <div className="flex flex-col gap-4">
+          <h1 className='text-center mt-7 font-semibold text-2xl' >Your Listings</h1>
+           {userListings.map((listing) => (
+        
+            <div key={listing._id} className='border rounded-lg p-3 flex justify-between items-center'>
+                <Link to={`/listing/${listing._id}`}>
+                  <img 
+                  src={listing.imageUrls[0]}
+                  alt='listed image'
+                  className='h-16 w-16 object-cover rounded-md '
+                  />
+                </Link >
+                <Link className='flex-1 text-slate-700 font-semibold mx-2
+                    hover:underline truncate' to={`/listing/${listing._id}`}>
+                    <p>{listing.name}</p>
+                </Link>
+                <div className='flex flex-col item-center'>
+                    <button className='text-red-700 uppercase font-semibold'>Delete</button>
+                    <button className='text-green-700 uppercase font-semibold'>Edit</button>
+                </div>
+            </div>
+        ))}           
+        </div>}
+      
     </div>
-  )
+  );
 }
 
 export default Profile
