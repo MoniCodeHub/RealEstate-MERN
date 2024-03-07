6d20cfbb508b05d634f1fa27af3e6c4cb4aebe18
moni <monishabaskararao@gmail.com>
2024-03-07 13:16:06 -0500

    set up the controller function for search listings

--- server-api/routes/listing_route.js
+++ server-api/routes/listing_route.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import express from "express";
-import { createListing, deleteListing, updateListing, getForEditListing } from "../controllers/listing.controller.js";
+import { createListing, deleteListing, updateListing, getForEditListing, getListings} from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -10,5 +10,6 @@ router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
 router.get("/getForEdit/:id", verifyToken, getForEditListing);
+router.get('/get', getListings);
 
 export default router;
