3a293e1b0a07b4b7dda6c4079b34176775d2272a
moni <monishabaskararao@gmail.com>
2024-03-08 10:52:35 -0500

    Created ListingItems components to show the listings

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect} from 'react'
 import {useNavigate} from'react-router-dom'
+import ListingItem from '../components/ListingItem';
 
 export default function Search() {
 
@@ -199,6 +200,24 @@ export default function Search() {
         <div className="">
             <h1 className='text-3xl font-semibold border-b p-3
             text-slate-700 mt-5'>Listing Results</h1>
+            <div className='p-7 flex-1 flex-wrap gap-4'>
+                {!loading && listings.length === 0 && (
+                    <p className='text-xl text-slate-700'>No listing found!</p>
+                )}
+
+                {
+                    loading && (
+                        <p className='text-xl text-slate-700 text-center w-full'>Loading...</p>
+                    )
+                }
+
+                {
+                    !loading && listings && listings.map((listing)=>(
+                    <ListingItem key={listing._id} listing={listing}/>
+                    
+                ))}
+
+            </div>
         </div>
     </div>
   )
