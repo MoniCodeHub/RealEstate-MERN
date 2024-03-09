361e46f81cfad77fe2241462af80d069f18f3186
moni <monishabaskararao@gmail.com>
2024-03-08 21:14:36 -0500

    Configured  for 'build' for deploment on render

--- server-api/index.js
+++ server-api/index.js
@@ -7,6 +7,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing_route.js";
 import cookieParser from "cookie-parser";
+import path from 'path';
 dotenv.config();
 
 mongoose
@@ -18,6 +19,8 @@ mongoose
     console.log(err);
   });
 
+  const __dirname = path.resolve();
+
 const app = express();
 app.use(express.json());
 
@@ -28,6 +31,12 @@ const PORT = 5000;
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res)=> {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
  
 
 // middleware to handle error in routing
